6e832203f2d952459904ebb4220e448a4c5ea1c0
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-04-22 13:14:01 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5175;
+
+app.use(cors())
+app.use(express.json());
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_PASSWORD}@cluster0.ce00xrg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+      
+      await client.connect();
+
+
+      const coffeeCollection  = client.db('coffeeDB').collections("coffee");
+      app.post('/coffee',async(req,res) => {
+        const newCoffee = req.body;
+        console.log(newCoffee);
+        
+        const result = await coffeeCollection.insertOne(newCoffee);
+        // res.json({ message: 'Coffee added successfully' });
+        res.send(result);
+      })
+      
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+app.get('/',(req,res) => {
+    res.send("coffee making server is running");
+})
+
+app.listen(port,() => {
+    console.log(`coffee server is running on port: ${port}`);
+})
